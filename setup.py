6ae499ca0355e518f80c6f84194b110e45dc6423
Jared Bunting <jared.bunting@peachjean.com>
2014-12-31 12:26:05 -0600

    Adding initial setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dnsfiles',
+      version='0.1.0',
+      description='Managing GANDI DNS from the file system.',
+      author='Jared Bunting',
+      author_email='jared.bunting@peachjean.com',
+      url='https://github.com/peachjean/dnsfiles',
+      packages=[],
+     )
