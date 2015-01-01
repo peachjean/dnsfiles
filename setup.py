b0abefcdc00a7a3f07aaa3e8d792c78b0f435183
Jared Bunting <jared.bunting@peachjean.com>
2015-01-01 11:19:59 -0600

    Adding versioneer setup.

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import versioneer
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'dnsfiles/_version.py'
+versioneer.versionfile_build = 'dnsfiles/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'dnsfiles-' # dirname like 'myproject-1.2.0'
 
 setup(name='dnsfiles',
-      version='0.2.0',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='Managing GANDI DNS from the file system.',
       author='Jared Bunting',
       author_email='jared.bunting@peachjean.com',
